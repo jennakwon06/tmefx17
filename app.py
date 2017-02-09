dd52fb9b83fa0a913f4eaff65f9159bfd1040367
Jenna Kwon <jennakwon@Jennas-MacBook-Pro.local>
2017-02-08 21:28:15 -0800

    crime stuff

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def fulton():
     return render_template("index.html")
 
 
+@app.route("/crime")
+def crime():
+    return render_template("crime.html")
+
+
 if __name__ == '__main__':
     app.run()
